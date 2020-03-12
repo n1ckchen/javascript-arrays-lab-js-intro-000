ce40252eae2739675ea049bd2b5376eb8edcd003
nick chen <nick.chen.pro@gmail.com>
2020-03-12 23:40:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name) {
   kittens.push(name);
   return kittens;
-}
+};
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+};
