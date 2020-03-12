6bc98eef6fcbe881a215590cea3014bfe8a4e997
nick chen <nick.chen.pro@gmail.com>
2020-03-12 23:47:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyPrependKitten(name) {
   return kittens;
 };
 
-function destructivelyRemoveLastKitten(name) {
+function destructivelyRemoveLastKitten() {
   kittens.pop();
   return kittens;
 };
